add2bf2a136adcccdc1e372d5feeb86b26fe9e59
Karsten Roethig <karsten.roethig@googlemail.com>
2013-11-23 17:26:58 +0100

    added example for JAXB

--- README.md
+++ README.md
@@ -30,3 +30,7 @@ maven-example-5
 * Barcodes with [Barbecue]{http://barbecue.sourceforge.net}
 * Barcode Interleave 2 of 5
 * Unit- and Integration-Test with [Maven Surefire Plugin]{http://maven.apache.org/surefire/maven-surefire-plugin/index.html} and [Maven Failsafe Plugin]{http://maven.apache.org/surefire/maven-failsafe-plugin/index.html}
+
+maven-example-6
+---------------
+* JAXB (source generation, marshal, unmarshal)
---
+++ maven-example-6/src/test/java/karstenroethig/maven/example6/JaxbTest.java
@@ -0,0 +1,66 @@
+package karstenroethig.maven.example6;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import junit.framework.TestCase;
+import karstenroethig.maven.example6.jaxb.Issue;
+import karstenroethig.maven.example6.jaxb.Issues;
+import karstenroethig.maven.example6.jaxb.Project;
+
+public class JaxbTest extends TestCase {
+
+	public void testMarshal() throws Exception {
+		
+		Project project = new Project();
+		
+		project.setKey( "KEY" );
+		project.setName( "projectname" );
+		
+		Issues issues = new Issues();
+		List<Issue> issuesList = issues.getIssue();
+		
+		Issue issue = new Issue();
+		
+		issue.setNumber( new BigInteger( "1" ) );
+		issue.setSummary( "Summary" );
+		issue.setDescription( "Description" );
+		
+		issuesList.add( issue );
+		
+		issue = new Issue();
+		
+		issue.setNumber( new BigInteger( "2" ) );
+		issue.setSummary( "Summary" );
+		issue.setDescription( "Description" );
+		
+		issuesList.add( issue );
+		
+		project.setIssues( issues );
+		
+		/*
+		 * Marshal
+		 */
+		JAXBContext context = JAXBContext.newInstance( Project.class );
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
+
+        File file = new File( "target/" + project.getName() + ".xml" );
+        
+        marshaller.marshal( project, file );
+        
+        /*
+         * Unmarshal
+         */
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        
+        Project proj = (Project)unmarshaller.unmarshal( file );
+        
+        System.out.println( "Porject: " +  proj.getName() + " (key: " + proj.getKey() + ")" );
+	}
+}
